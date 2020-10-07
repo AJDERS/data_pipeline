db9b7f8da611444314fabe72ea0833b3ebfa153e
ajders <ajpofficial@gmail.com>
2020-10-07 15:56:24 +0200

    Updated callback to be agnostic about logged variables

--- callback.py
+++ callback.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 
 class Callback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if (logs.get('accuracy') > 0.998) or (logs.get('val_accuracy') > 0.95):
+        if any([logs.get(k) > 0.99 for k in logs.keys()]):
             self.model.stop_training = True
+            self.reason_to_stop = 'Achieved too high metrics, stopping to avoid overfitting.'
