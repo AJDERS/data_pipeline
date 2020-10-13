75925f052d07ba9383e836fc64a9a4160a0a27a6
ajders <ajpofficial@gmail.com>
2020-10-13 14:16:12 +0200

    Small function for cleaning storage directory

---
+++ util/clean_storage_folder.py
@@ -0,0 +1,6 @@
+import shutil
+import make_storage_folder
+
+def clean_storage(storage_directory):
+    shutil.rmtree(storage_directory)
+    make_storage_folder.make_storage_folder(storage_directory)
