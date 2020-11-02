b2bc00b65bd9d415b051d8695ef205ebb1f8935c
ajders <ajpofficial@gmail.com>
2020-11-02 11:22:57 +0100

    Added early stopping and tensorboard callbacks, split logs into tensorboard-log and general execution log

--- util/clean_storage_folder.py
+++ util/clean_storage_folder.py
@@ -4,3 +4,10 @@ from . import make_storage_folder
 def clean_storage(storage_directory):
     shutil.rmtree(storage_directory)
     make_storage_folder.make_storage_folder(storage_directory)
+
+def clean_logs(log_directory):
+    try:
+        shutil.rmtree(log_directory)
+    except FileNotFoundError:
+        pass
+    make_storage_folder.make_logs_folder(log_directory)
