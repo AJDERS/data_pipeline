1201fc0a65b17750d34142e4c00c803f293c4336
ajders <ajpofficial@gmail.com>
2020-11-03 13:33:03 +0100

    Minor changes

--- models/unet3d.py
+++ models/unet3d.py
@@ -62,10 +62,11 @@ def _resnet_block(
     # wrn dropout
     if dropout_wrn and dropout_rate is not None:
         tensor = Dropout(rate=dropout_rate)(tensor)
-
+    
     # BN
     if batchnorm:
         tensor = BatchNormalization()(tensor)
+
 
     # Activation
     tensor = _activation(tensor, activation, name+'_2')
@@ -77,6 +78,7 @@ def _resnet_block(
         padding=padding,
         kernel_initializer=initializer
     )(tensor)
+
 
     # shortcut
     if shortcut:
@@ -106,6 +108,7 @@ def _down_block(
     ):
 
     tensor = Conv3D(kernel_size=1, filters=num_filters)(tensor)
+
     tensor = _resnet_block(
         tensor,
         num_filters,
@@ -151,7 +154,6 @@ def _down_block(
         )(tensor)
 
         tensor = AvgPool2D(strides)(tensor)
-
     else:
         raise ValueError(
             'down_sampling should be one of [ \'strided_conv\', \'maxpool\' ]'
@@ -227,8 +229,9 @@ def _up_block(
             )(tensor)
 
         else:
+
             tensor = Conv3D(
-                filters=num_filters*scale[0]*scale[1],
+                filters=num_filters*scale[0]**scale[1],
                 kernel_size=1,
                 padding=padding,
                 kernel_initializer=initializer
@@ -236,12 +239,13 @@ def _up_block(
 
             tensor = SubPixel3D(scale=scale)(tensor)
 
+
     elif up_sampling == 'simple_subpixel':
         tensor = SubPixel3D(scale=scale)(tensor)
 
     elif up_sampling == 'conv_subpixel':
         tensor = Conv3D(
-            filters=num_filters*scale[0],
+            filters=num_filters*scale[0]*scale[1],
             kernel_size=1,
             padding=padding,
             kernel_initializer=initializer
@@ -253,6 +257,7 @@ def _up_block(
             'up_sampling should be one of '
             '[ \'simple\', \'transposed\', \'subpixel\' ]'
         )
+
     return tensor
 
 def create_model(conf):
@@ -310,6 +315,7 @@ def create_model(conf):
 
     # Encoder layer 1,
     #                           ts/2 x ts/2 x 64 > ts/4 x ts/4 x 128
+
     e1, s1 = _down_block(
         e0,
         num_filters=num_filters*2,
@@ -347,7 +353,7 @@ def create_model(conf):
     #                           ts/8 x ts/8 x 256 > ts/8 x ts/8 x 512
     conv0 = _resnet_block(
         e2,
-        num_filters=num_filters*16, # 8
+        num_filters=num_filters*8, # 8
         activation=activation,
         initializer=initializer,
         dropout_rate=dropout_rate,
@@ -359,8 +365,8 @@ def create_model(conf):
     # Decoder layer 0,
     #                           ts/8 x ts/8 x 512 > ts/4 x ts/4 x 128 > ts/4 x ts/4 x 256
     d0 = _up_block(
-        e2, ### e2???
-        num_filters=num_filters*8, # 4
+        conv0, ### e2???
+        num_filters=num_filters*4, # 4
         activation=activation,
         initializer=initializer,
         conv_1d_later=conv_1d_later,
@@ -382,7 +388,7 @@ def create_model(conf):
     #                           ts/4 x ts/4 x 256 > ts/2 x ts/2 x 64 > ts/2 x ts/2 x 128
     d1 = _up_block(
         d0,
-        num_filters=num_filters*4, # 2
+        num_filters=num_filters*2, # 2
         activation=activation,
         initializer=initializer,
         conv_1d_later=conv_1d_later,
@@ -394,7 +400,7 @@ def create_model(conf):
         batchnorm=batchnorm,
         name='up_block_2'
     )
-    
+
     if skip_connection == 'concat':
         Concatenate(axis=-1, name='concat_skip_2')([s1, d1])
     elif skip_connection == 'add':
@@ -404,7 +410,7 @@ def create_model(conf):
     #                           ts/2 x ts/2 x 128 > 2*ts x2*ts x 64 > 2*ts x 2*ts x 64
     d2 = _up_block(
         d1,
-        num_filters=num_filters*2, # 1
+        num_filters=num_filters, # 1
         activation=activation,
         initializer=initializer,
         conv_1d_later=conv_1d_later,
