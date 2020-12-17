59d4b599c55f66332b204a2dc242c84e81a70d9c
ajders <ajpofficial@gmail.com>
2020-12-17 14:37:05 +0100

    Pipeline for training rnns

--- train_rnn.py
+++ train_rnn.py
@@ -1,14 +1,22 @@
+import os
 import random
+import logging
 import importlib
 import configparser
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from typing import Type
+from datetime import datetime
 from models.feedback import Feedback
+from util import clean_storage_folder as cleaner
 from util.sliding_window import SlidingWindow
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from tensorflow.python.keras import backend
-
+from tensorflow.keras.callbacks import History
 
 class Compiler():
 
@@ -16,11 +24,27 @@ class Compiler():
         self.config = configparser.ConfigParser()
         self.config.read(config_path)
         self.config_path = config_path
+        self.data_folder_path = data_folder_path
+        self.train_path = os.path.join(self.data_folder_path, 'training')
+        self.valid_path = os.path.join(self.data_folder_path, 'validation')
+        self.eval_path = os.path.join(self.data_folder_path, 'evaluation')
         self.windows = SlidingWindow(data_folder_path, config_path)
-        self.loaded_model = False
-        self.build_model()
+        self._set_runtime_variables()
         self._allocate_memory()
         self._reset_seeds()
+        self._make_logs()
+
+    def _set_runtime_variables(self):
+        self.train_X = None
+        self.train_Y = None
+        self.valid_X = None
+        self.valid_Y = None
+        self.eval_X = None
+        self.eval_Y = None
+        self.loaded_model = False
+        self.fitted = False
+        self.model_compiled = False
+
 
     # Utility functions for TF
     def _allocate_memory(self):
@@ -46,16 +70,39 @@ class Compiler():
         random.seed(seed)
         tf.compat.v1.set_random_seed(seed)
         print("RANDOM SEEDS RESET") 
+    
+    def _clean_log_dir(self):
+        cleaner.clean_logs('output/logs')
+
+    def _make_logs(self):
+        self._clean_log_dir()
+        now = datetime.now()
+        self.dt_string = now.strftime("%Y%m%d-%H%M%S")
+        logging.basicConfig(
+                    filename=f'output/logs/model_{self.dt_string}.log',
+                    level=logging.INFO
+        )
 
 
-    def _make_windows(self):
+    def _make_windows(self, mode):
+        assert mode in ['training', 'validation', 'evaluation'], 'Invalid mode.'
         threshold = self.config['RNN'].getint('CandidateThreshold')
-        self.windows._slinding_windows(self.windows.train_tracks, threshold)
-        self.windows._slinding_windows_labels(self.windows.train_tracks)
-        return self.windows._organize_windows()
+        if mode == 'training':
+            self.windows._slinding_windows(self.windows.train_tracks, threshold)
+            self.windows._slinding_windows_labels(self.windows.train_tracks)
+            return self.windows._organize_windows()
+        elif mode == 'validation':
+            self.windows._slinding_windows(self.windows.val_tracks, threshold)
+            self.windows._slinding_windows_labels(self.windows.val_tracks)
+            return self.windows._organize_windows()
+        else:
+            self.windows._slinding_windows(self.windows.eval_tracks, threshold)
+            self.windows._slinding_windows_labels(self.windows.eval_tracks)
+            return self.windows._organize_windows()
 
 
-    def build_model(self) -> None:
+
+    def build_model(self, batch_size=None, window_size=None, features=None) -> None:
         """
         **This method builds a model if none is loaded.**
 
@@ -63,9 +110,9 @@ class Compiler():
         ``self.loaded_model == True``, see note of ``Model.load_model``.
         """
 
-        model_name = self.config['MODEL'].get('ModelName')
+        model_name = self.config['RNN'].get('ModelName')
         tmp = model_name.split('.')
-        module = '.'.join(tmp[:1])
+        module = '.'.join(tmp[:2])
         class_ = tmp[-1]
         if not self.loaded_model:
             print('Building model... \n')
@@ -73,30 +120,152 @@ class Compiler():
                 module,
                 package=None
             )
-            self.model = getattr(model, class_)
+            if self.config['RNN'].getboolean('CustomModel'):
+                self.model = getattr(model, class_)
+            else:
+                class_ = getattr(model, class_)
+                model = class_(
+                    self.config_path,
+                    batch_size,
+                    window_size,
+                    features
+                )
+                self.model = model.create_model()
         else:
             print('Model is already loaded.')
 
-    def compile_and_fit(self):
-        X, Y = self._make_windows()
-        patience = self.config['TRAINING'].getint('Patience')
-        epochs = self.config['TRAINING'].getint('Epochs')
+    def _load_data(self):
+        self.train_X, self.train_Y = self._make_windows(mode='training')
+        self.valid_X, self.valid_Y = self._make_windows(mode='validation')
+        self.eval_X, self.eval_Y = self._make_windows(mode='evaluation')
 
-        early_stopping = EarlyStopping(
-            monitor='val_loss',
-            patience=patience,
-            mode='min'
+    def compile_and_fit(self):
+        self._load_data()
+
+        features = self.train_X.shape[-1]
+        self.build_model(
+            self.windows.batch_size,
+            self.windows.window_size,
+            features
         )
+        self.model.summary()
+        epochs = self.config['TRAINING'].getint('Epochs')
+        learning_rate = self.config['TRAINING'].getfloat('LearningRate')
+
+        cp_callback, early_stopping = self._checkpoints()
         self.model.compile(
             loss='mean_squared_error',
-            optimizer=Adam(),
+            optimizer=Adam(learning_rate=learning_rate),
             metrics=['mean_squared_error'],
             run_eagerly=True
         )
         history = self.model.fit(
-            X,
+            self.train_X,
+            self.train_Y,
             epochs=epochs,
-            validation_data=Y,
-            callbacks=[early_stopping]
+            callbacks=[cp_callback, early_stopping],
+            batch_size=self.config['PREPROCESS_TRAIN'].getint('BatchSize'),
+            verbose=1,
+            validation_data=(self.valid_X, self.valid_Y),
         )
+        self.fitted = True
+        self.illustrate_history(history)
+        self.model.save(f'model_{self.dt_string}')
+        backend.clear_session()
         return history
+
+    def illustrate_history(self,
+        history: Type[History]) -> None:
+        """
+        **Generates plots of metrics over epochs.**
+
+        The metrics over epochs plots are saved to 
+        ``output/{metric}_{dt_string}.png``
+
+        :param history: A ``tensorflow.keras.History`` object. 
+            Its `History.history` attribute is a record of training loss values
+            and metrics values at successive epochs, as well as validation loss
+            values and validation metrics values (if applicable).
+        """
+        # Get metrics which are not `val_{metric}`:
+        metrics = [key for key in history.history.keys() if '_' not in key]
+        plt.figure()
+        for key in metrics:
+            plt.plot(history.history[f'{key}'])
+            plt.plot(history.history[f'val_{key}'])
+            plt.title(f'model {key}')
+            plt.ylabel(f'{key}')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'test'], loc='upper left')
+            plt.savefig(f'output/{key}_{self.dt_string}.png')
+
+
+    def predict(self, mode: str) -> list:
+        assert self.fitted, 'Model is not fitted.'
+        assert mode in ['training', 'validation', 'evaluation']
+
+        if mode == 'training':
+            if self.train_X is None:
+                self.train_X, self.train_Y = self._make_windows(mode='training')
+            data = self.train_X
+            label = self.train_Y
+        elif mode == 'validation':
+            if self.valid_X is None:
+                self.valid_X, self.valid_Y = self._make_windows(mode='validation')
+            data = self.valid_X
+            label = self.valid_Y
+        elif mode == 'evaluation':
+            if self.eval_X is None:
+                self.eval_X, self.eval_Y = self._make_windows(mode='evaluation')
+            data = self.eval_X
+            label = self.eval_Y
+
+        batch_size = self.config['PREPROCESS_TRAIN'].getint('BatchSize')
+        batch = data[0:batch_size]
+        label_batch = label[0:batch_size]
+        predicted_batch = self.model.predict(batch)
+        self.predicted_data = True
+        return batch, predicted_batch, label_batch
+
+
+    def load_model(self, model_path: str) -> None:
+        """**This function loads a model from a given path.**
+
+        :param model_path: A directory containing a tensforflow models object.
+        :type model_path: ``str``.
+        :returns: A Keras model instance.
+        :rtype: ``tensorflow.keras.model.Model``.
+
+        .. note:: If a model is loaded ``self.model`` is set to ``True``.
+        """
+        try:
+            reconstructed_model = keras.models.load_model(model_path)
+            self.loaded_model = True
+            self.fitted = True
+            reconstructed_model.summary()
+            self.model = reconstructed_model
+        except FileNotFoundError:
+            return FileNotFoundError
+
+    def _load_from_checkpoint(self, checkpoint_path: str) -> None:
+        latest = tf.train.latest_checkpoint(checkpoint_path)
+        self.build_model()
+        self.fitted = True
+        self.model.load_weights(latest)
+
+    def _checkpoints(self):
+        checkpoint_path = "./output/checkpoints/cp-{epoch:04d}.ckpt"
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(
+           checkpoint_path,
+           verbose=1,
+           save_weights_only=True,
+           save_best_only=True
+        )
+        patience = self.config['TRAINING'].getint('Patience')
+        early_stopping = EarlyStopping(
+            monitor='val_loss',
+            mode='min',
+            verbose=1,
+            patience=patience)
+
+        return cp_callback, early_stopping
--- util/sliding_window.py
+++ util/sliding_window.py
@@ -147,39 +147,26 @@ class SlidingWindow:
                             y.append(ys[y_index][3])
             X.append(np.array(x))
             Y.append(np.array(y))
-        X, Y = self._make_input(X, Y)
+        X = self._pad_organise(X)
+        Y = self._pad_organise(Y)
         return X, Y
 
+    def _cap_values(self, array):
+        cap = self.config['DATA'].getint('TargetSize')
+        array[(array < 0) | (array > (cap-1))] = -1
+        return array
 
-    def _make_input(self, inputs, labels):
-        """
-        Seems complicated, but is to ensure, that we get output shape:
-        [batch_size, window_size, 2, channel, (candidate)]
-        where (candidate) is a ragged dimension.
-        This is to better fit with LSTM/RNN input conventions.
-        The `RaggedTensor` API is lackluster to say the least,
-        so this is a hacky version to get the correct dimensions, while
-        using the `RaggedTensor` functionalities when training.
-
-        """
-        x0 = []
-        y0 = []
-        for x in range(len(inputs)):
-            for i in range(inputs[0].shape[2]):
-                for j in range(inputs[0].shape[1]):
-                    for k in range(inputs[0].shape[-1]):
-                        x0.append(inputs[x][:,j,i,k])
-                        y0.append(labels[x][:,j,i,k])
-        
-        x1 = tf.ragged.constant(x0)
-        x2 = tf.RaggedTensor.from_uniform_row_length(x1, 1)
-        x3 = tf.RaggedTensor.from_uniform_row_length(x2, 2)
-        x4 = tf.RaggedTensor.from_uniform_row_length(x3, self.window_size)
-        y1 = tf.ragged.constant(y0)
-        y2 = tf.RaggedTensor.from_uniform_row_length(y1, 1)
-        y3 = tf.RaggedTensor.from_uniform_row_length(y2, 2)
-        y4 = tf.RaggedTensor.from_uniform_row_length(y3, self.window_size)
-        return x4, y4
+    def _pad_organise(self, array):
+        array = tf.keras.preprocessing.sequence.pad_sequences(
+            array,
+            padding='post',
+            value=-1
+        )
+        array = self._cap_values(array)
+        array = np.stack(array, axis=0)
+        array = np.reshape(array, (-1, 2, self.window_size))
+        array = np.transpose(array, (0, 2, 1))
+        return array
 
     def _append_data(self, time, window):
         """
