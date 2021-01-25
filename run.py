2f1a121feda8f5f7fb4cf4154b25fd65af639236
ajders <ajpofficial@gmail.com>
2021-01-25 14:55:56 +0100

    Minor changes

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ from util import prepare_for_runs
 from train_rnn import Compiler
 
 def main():
-    #prepare_for_runs.main()
+    prepare_for_runs.main()
     configs = [f for f in os.listdir('config/active_configs') if os.path.isfile(os.path.join('config/active_configs', f))]
     for config in configs:
-        C = Compiler('storage', config)
+        if config == '__init__.py':
+            continue
+        print(config)
+        C = Compiler('storage', os.path.join('config/active_configs', config))
         C.compile_and_fit()
         C.evaluate()
 
