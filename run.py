ad3326f274be10c6de1e41f6f1765dfb5f5599a5
ajders <ajpofficial@gmail.com>
2021-01-25 14:32:23 +0100

    Removed runs folder and made script instead

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import os
+from util import prepare_for_runs
 from train_rnn import Compiler
 
 def main():
+    #prepare_for_runs.main()
     configs = [f for f in os.listdir('config/active_configs') if os.path.isfile(os.path.join('config/active_configs', f))]
     for config in configs:
         C = Compiler('storage', config)
         C.compile_and_fit()
         C.evaluate()
-    
 
 if __name__ == "__main__":
     main()
